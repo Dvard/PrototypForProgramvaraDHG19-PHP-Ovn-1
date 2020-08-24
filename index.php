894219119797bc2d5875a7b7e0e923bb295781ef
Edvard DHG19 <56823476+Dvard@users.noreply.github.com>
2020-08-24 11:04:14 +0300

    Update index.php

--- index.php
+++ index.php
@@ -6,14 +6,14 @@ $website = '<a href="https://prakticum.fi">webbsida</a>'
 ?>
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+	<head>
 		<meta charset="utf-8">
 		<title>PHP-Övning 1</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	</head>
 	<body>
-        <p>>Skola: <?php echo $school?></p>
-        <p>Multiplakation: <?php echo $number * $decimal?></p>
-        <p>Website: <?php echo $website?></p>
+		<p>>Skola: <?php echo $school?></p>
+		<p>Multiplakation: <?php echo $number * $decimal?></p>
+		<p>Website: <?php echo $website?></p>
 	</body>
 </html>
