b20085acda021d5d893e5776d14ad0c9ccbf576b
Edvard DHG19 <56823476+Dvard@users.noreply.github.com>
2020-08-24 11:03:10 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+$school = 'Practicum';
+$number = 3;
+$decimal = 5.1;
+$website = '<a href="https://prakticum.fi">webbsida</a>'
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+		<meta charset="utf-8">
+		<title>PHP-Övning 1</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	</head>
+	<body>
+        <p>>Skola: <?php echo $school?></p>
+        <p>Multiplakation: <?php echo $number * $decimal?></p>
+        <p>Website: <?php echo $website?></p>
+	</body>
+</html>
